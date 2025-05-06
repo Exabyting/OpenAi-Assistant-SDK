6c6174078934efc2e25238ad9298f408e420e817
exaAshik <167954881+exaAshik@users.noreply.github.com>
2025-05-06 10:31:58 +0600

    Update MessageRequest.java

--- OpenAI-SDK/src/main/java/org/com/model/MessageRequest.java
+++ OpenAI-SDK/src/main/java/org/com/model/MessageRequest.java
@@ -5,16 +5,19 @@ public class MessageRequest {
     private String role;
     private String content;
 
-    public MessageRequest(String role, String content) {
-        this.role = role;
-        this.content = content;
-    }
-
     public String getRole() {
         return role;
     }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     public String getContent() {
         return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }

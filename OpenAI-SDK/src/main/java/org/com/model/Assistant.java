ab5da35dbedfc7addc04c3b5012fed4eb2e38170
exaAshik <167954881+exaAshik@users.noreply.github.com>
2025-05-06 10:26:08 +0600

    Update Assistant.java

--- OpenAI-SDK/src/main/java/org/com/model/Assistant.java
+++ OpenAI-SDK/src/main/java/org/com/model/Assistant.java
@@ -4,9 +4,49 @@ import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Assistant {
-    public String id;
-    public String object;
-    public String name;
-    public String model;
-    public Map<String, Object> additionalProperties;
+    private String id;
+    private String object;
+    private String name;
+    private String model;
+    private Map<String, Object> additionalProperties;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return additionalProperties;
+    }
+
+    public void setAdditionalProperties(Map<String, Object> additionalProperties) {
+        this.additionalProperties = additionalProperties;
+    }
 }

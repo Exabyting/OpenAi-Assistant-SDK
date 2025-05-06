24947b70a203586530ac5a0cd7e4d9361e039a01
exaAshik <167954881+exaAshik@users.noreply.github.com>
2025-05-06 10:28:10 +0600

    Update AssistantRequest.java

--- OpenAI-SDK/src/main/java/org/com/model/AssistantRequest.java
+++ OpenAI-SDK/src/main/java/org/com/model/AssistantRequest.java
@@ -11,4 +11,36 @@ public class AssistantRequest {
     public static class Tool {
         public String type;
     }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<Tool> getTools() {
+        return tools;
+    }
+
+    public void setTools(List<Tool> tools) {
+        this.tools = tools;
+    }
 }
